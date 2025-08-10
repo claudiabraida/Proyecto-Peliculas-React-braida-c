9c6cf3d5ee07243b98424eb6aaac90d98ab14467
claudiabraida <braidacreaciones@gamail.com>
2025-08-10 15:04:04 -0300

    feat: routes

--- src/router/RouterMovies.jsx
+++ src/router/RouterMovies.jsx
@@ -1,7 +1,18 @@
 /* ************** REACT ROUTER ************** */
 import { BrowserRouter, Routes,Route } from "react-router";
 
+/* *************** COMPONENTS *************** */
+import NavBar from "../components/1NavBar";
+import Footer from "../components/4Footer";
+
 /* ***************** VIEWS ***************** */
+import Home from "../views/1Home";
+import NewMovies from "../views/2NewMovies";
+import Popular from "../views/3Popular";
+import Search from "../views/4Search";
+import DetailMovies from "../views/5DetailMovies";
+import TrailerMovies from "../views/6TrailerMovies";
+import FavoritesMovies from "../views/7FavoritesMovies";
 
 /* ************************************** */
 export default function RouterMovies() {
@@ -9,6 +20,12 @@ export default function RouterMovies() {
     <BrowserRouter>
     <NavBar/>
       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="new-movies" element={<NewMovies/>}/>
+        <Route path="popular" element={<Popular/>}/>
+        <Route path="search" element={<Search/>}/>
+        <Route path="/movie/:id" element={<DetailMovies/>}/>
+        <Route path="/favorites-movies/:id" element={<FavoritesMovies/>}/>
         <Route path="*" element={<h2>ERROR 404 NOT FOUND</h2>} />
       </Routes>
       <Footer/>
--- src/views/6TrailerMovies.jsx
+++ src/views/6TrailerMovies.jsx
@@ -1,5 +1,5 @@
 
-export default function TrilerMovies() {
+export default function TrailerMovies() {
 
   return <>
     <h1>TRAILER</h1>
